a06dd3f50b6ad989f242a0be059887a3f8d602dd
Melissa Wallace <mhk33@cornell.edu>
2013-11-22 02:07:31 +0000

    syntax error?
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@36 29d2e8ee-217f-479a-8a37-9775d62f972e

--- template.php
+++ template.php
@@ -8,3 +8,5 @@ function bootstrap_subtheme_menu_link(array $variables) {
   unset($element['#localized_options']['attributes']['data-hover'] = 'dropdown';);
   unset($element['#localized_options']['attributes']['data-target'] = '#';);
 }
+
+?>
