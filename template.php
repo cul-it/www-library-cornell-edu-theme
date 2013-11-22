1a254654e7b522759cd0f2dc1a45bf33ae55a74f
Melissa Wallace <mhk33@cornell.edu>
2013-11-22 02:06:51 +0000

    fix parent link not working in dropdown menu
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@35 29d2e8ee-217f-479a-8a37-9775d62f972e

--- template.php
+++ template.php
@@ -3,3 +3,8 @@
 /**
  * @file template.php
  */
+
+function bootstrap_subtheme_menu_link(array $variables) {
+  unset($element['#localized_options']['attributes']['data-hover'] = 'dropdown';);
+  unset($element['#localized_options']['attributes']['data-target'] = '#';);
+}
