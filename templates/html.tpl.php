8e6b771e06c3e44e985cf78337ee5fc91bad078f
Melissa Wallace <mhk33@cornell.edu>
2013-12-05 15:50:10 +0000

    more debugging IE8 media queries problem
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@191 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
