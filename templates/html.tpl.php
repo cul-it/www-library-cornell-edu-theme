79008f8e2c3f4609b16a3b4321494ab6e5aa831a
Melissa Wallace <mhk33@cornell.edu>
2013-12-05 15:31:19 +0000

    external hosting didn't work; try reserving loading order so shiv and respond go after drupal scripts
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@190 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -9,12 +9,12 @@
   <title><?php print $head_title; ?></title>
 
   <?php print $styles; ?>
+  <?php print $scripts; ?>
   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <script src="/sites/all/themes/bootstrap_cul7/js/html5shiv.js"></script>
+    <script src="/sites/all/themes/bootstrap_cul7/js/respond.min.js"></script>
   <![endif]-->
-  <?php print $scripts; ?>
   
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
