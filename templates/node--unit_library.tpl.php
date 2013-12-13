d5eef52f824346bd8520f53f8c613dae4026bdaf
Melissa Wallace <mhk33@cornell.edu>
2013-12-13 20:37:48 +0000

    display body field only
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@314 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/node--unit_library.tpl.php
+++ templates/node--unit_library.tpl.php
@@ -92,18 +92,16 @@
 
   <?php if ($display_submitted): ?>
     <div class="submitted">
-      <?php print $submitted;
-  <?php endif; ?>      
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
 
-<!--   print render($content); -->
-  render($content['body'])
-
-  <div class="content"<?php print $co ?>
-    </div>ntent_attributes; ?>>
+  <div class="content"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      print render($content['body']);
     ?>
   </div>
 
