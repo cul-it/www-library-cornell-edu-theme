78e4393366b88a6e35c1e8c705152c1a28ef0a8f
Melissa Wallace <mhk33@cornell.edu>
2013-12-16 19:46:07 +0000

    add hours link
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@338 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/node--unit_library.tpl.php
+++ templates/node--unit_library.tpl.php
@@ -125,6 +125,7 @@
           <?php print render($content['group_contact']['field_circ_phone']); ?>
           <h4>Website <i class="fa fa-external-link"></i></h4>
           <?php print render($content['group_contact']['field_website']); ?>
+          <p><a href="/hours">Library hours</a></p>
         </div>
       </div>
   </div>
