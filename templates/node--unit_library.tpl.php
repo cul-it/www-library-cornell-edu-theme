e0d755660d2ea768f2d2e2b40ff3b94e2c609e4d
Melissa Wallace <mhk33@cornell.edu>
2013-12-18 16:45:38 +0000

    add hours header
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@366 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/node--unit_library.tpl.php
+++ templates/node--unit_library.tpl.php
@@ -120,6 +120,7 @@
           <?php print render($content['group_contact']['field_circ_phone']); ?>
           <h4>Website <i class="fa fa-external-link"></i></h4>
           <?php print render($content['group_contact']['field_website']); ?>
+          <h4>Hours</h4>
           <?php print render($content['field_hours']); ?>
         </div>
       </div>
