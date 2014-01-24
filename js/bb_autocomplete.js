9ac90631a59ba27a0e5108593c13e2c297e35548
Matt Connolly <mjc12@cornell.edu>
2014-01-24 21:20:30 +0000

    Fix basic autocomplete (changes didn't get committed before).
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@574 29d2e8ee-217f-479a-8a37-9775d62f972e

--- js/bb_autocomplete.js
+++ js/bb_autocomplete.js
@@ -1,16 +1,40 @@
+// (function ($) {
+// 	$(document).ready(function() {
+
+// 		$('#q').autocomplete({
+// 			source: Drupal.settings.basePath + 'sites/all/themes/' + Drupal.settings.ajaxPageState.theme + '/autocomplete.php',
+//       minLength: 2,
+//       select: function(event, ui) {
+//         var url = ui.item.id;
+//         if (url != '#') {
+//           location.href = url;
+//         }
+//       }
+//     })
+//     .data('ui-autocomplete')._renderItem = function(ul, item) {
+//       return $('<li>')
+//         .data('ui-autocomplete-item', item)
+//         .append('<a>' + item.label + '</a><i class="fa fa-external-link"></i>')
+// //        .append()
+//         .appendTo(ul);
+
+//     }
+
+// 	});
+// })(jQuery);
+
 (function ($) {
-	$(document).ready(function() {
-		$('#q').click(function() { console.log('clicked!'); });
-		$('#q').autocomplete({
-			source: 'autocomplete.php',
-        	minLength: 2,
-        	select: function(event, ui) {
-        		console.log('got response');
-          		var url = ui.item.id;
-          		if (url != '#') {
-          			location.href = url;
-          		}
-        	}
-     	});
-	});
+  $(document).ready(function() {
+    $('#q').autocomplete({
+      source: Drupal.settings.basePath + 'sites/all/themes/' + Drupal.settings.ajaxPageState.theme + '/autocomplete.php',
+          minLength: 2,
+          select: function(event, ui) {
+            console.log('got response');
+              var url = ui.item.id;
+              if (url != '#') {
+                location.href = url;
+              }
+          }
+      });
+  });
 })(jQuery);
