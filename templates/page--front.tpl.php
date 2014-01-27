71092edbbd6993d9a64844d59147b66de20173f8
Melissa Wallace <mhk33@cornell.edu>
2014-01-27 20:40:23 +0000

    update catalog link to newcatalog.library
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@585 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -73,7 +73,7 @@
             <div class="search-options">
               <!-- <h2>Other search options</h4> -->
               <ul>
-                <li><a href="http://search.library.cornell.edu"><i class="fa fa-external-link"></i> Catalog</a></li>
+                <li><a href="http://newcatalog.library.cornell.edu"><i class="fa fa-external-link"></i> Catalog</a></li>
                 <li><a href="http://cornell.summon.serialssolutions.com/search?s.cmd=addFacetValueFilters(ContentType,Journal%20Article)"><i class="fa fa-external-link"></i> Articles</a></li>
                 <li><a href="http://erms.library.cornell.edu/search~S4"><i class="fa fa-external-link"></i> Database Names</a></li>
                 <li><a href="http://erms.library.cornell.edu/"><i class="fa fa-external-link"></i> E-Journal Titles</a></li>
