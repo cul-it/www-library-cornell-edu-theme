e11c04d0b0e863db1e45d9f8f2aa6a9a517d3d14
Melissa Wallace <mhk33@cornell.edu>
2014-02-04 14:22:27 +0000

    back out of accessiblity social icon stuff because it didn't work
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@677 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -175,8 +175,8 @@
       </div>
       <div class="footer-secondary">
         <div class="social-links">
-          <a class="facebook-button" href="http://www.facebook.com/cornelluniversitylibrary"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i>Find us on Facebook</a> 
-          <a class="twitter-button" href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.library.cornell.edu%2Findex&amp;region=follow_link&amp;screen_name=Cornell_Library&amp;tw_p=followbutton&amp;variant=2.0"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i>Follow us on Twitter</a>
+          <a class="facebook-button" href="http://www.facebook.com/cornelluniversitylibrary" title="Find us on Facebook"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i></a> 
+          <a class="twitter-button" href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.library.cornell.edu%2Findex&amp;region=follow_link&amp;screen_name=Cornell_Library&amp;tw_p=followbutton&amp;variant=2.0" title="Follow us on Twitter"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i></a>
         </div>
         <?php print render($page['footer-secondary']); ?>
       </div>
