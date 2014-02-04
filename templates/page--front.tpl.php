cf63cfa685451266f6913254b11b18316f537295
Melissa Wallace <mhk33@cornell.edu>
2014-02-04 18:31:45 +0000

    oops add alt tags for different image
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@679 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -5,8 +5,8 @@
     <div class="row">
       <div class="cornell-logo">
         <div class="cu-logo">
-          <img src="/sites/all/themes/bootstrap_cul7/img/layout/cul-logo-white.gif">
-          <a id="insignia-link" href="http://www.cornell.edu/" alt="Cornell insignia">Cornell Insignia</a>
+          <img src="/sites/all/themes/bootstrap_cul7/img/layout/cul-logo-white.gif" alt="Cornell University Library">
+          <a id="insignia-link" href="http://www.cornell.edu/">Cornell Insignia</a>
           <div class="unit-signature-links">
             <a id="cornell-link" href="http://www.cornell.edu/">Cornell University</a>
             <a id="unit-link" href="/">Cornell University Library</a>
