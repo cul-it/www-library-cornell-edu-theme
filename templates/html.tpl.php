04e265f141fd718500b7629cd2a418a12d4441d8
Melissa Wallace <mhk33@cornell.edu>
2014-02-12 18:00:01 +0000

    prevent zoom into search box on iOS
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@692 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -3,7 +3,8 @@
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
   <meta name="description" content="">
   <meta name="author" content="">
   <title><?php print $head_title; ?></title>
