a793c420de5a9dd6e48c732ac24a27f4d743c75b
Melissa Wallace <mhk33@cornell.edu>
2014-05-30 23:32:36 +0000

    add search icon and separate search box for mobile
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@777 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -28,13 +28,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+      <button type="button" class="search-toggle" data-toggle="collapse" data-target=".search-collapse">
+        <i class="fa fa-search"></i>
+      </button>
       <div class="mobile-insignia visible-xs">
         <a href="http://www.cornell.edu"><img src="/sites/all/themes/bootstrap_cul7/img/layout/insignia.png" alt="Cornell University"></a>
       </div>
       <a class="navbar-brand visible-xs" href="/">Cornell University Library</a>
     </div>
-    <div class="navbar-collapse collapse">
-      <div class="visible-xs mobile-search">
+    <div class="visible-xs mobile-search">
+      <div class="search-collapse collapse">
         <form class="form-inline clearfix" role="form" action="http://search.library.cornell.edu/" method="get">
           <div class="form-group">
             <label class="sr-only" for="q">Search</label>
@@ -43,6 +46,8 @@
           <button type="submit" class="btn btn-search-icon btn-sm"><i class="fa fa-search fa-inverse"></i></button>
         </form>
       </div>
+    </div>
+    <div class="navbar-collapse collapse">
       <div class="nav navbar-nav">
         <?php print render($page['navigation']); ?>
       </div>
