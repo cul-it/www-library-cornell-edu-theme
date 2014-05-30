9d72cb8bfeea75aef820aae82b8df9dc591194fa
Melissa Wallace <mhk33@cornell.edu>
2014-05-30 19:11:57 +0000

    add custom markup from page.tpl.php
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@770 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/maintenance-page.tpl.php
+++ templates/maintenance-page.tpl.php
@@ -25,72 +25,152 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>">
-  <div id="page">
-    <div id="header">
-      <div id="logo-title">
+  
 
-        <?php if (!empty($logo)): ?>
-          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
+  <div id="skiptocontent"><a href="#maincontent">skip to main content</a></div>
 
-        <div id="name-and-slogan">
-          <?php if (!empty($site_name)): ?>
-            <h1 id="site-name">
-              <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-
-          <?php if (!empty($site_slogan)): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /name-and-slogan -->
-      </div> <!-- /logo-title -->
-
-      <?php if (!empty($header)): ?>
-        <div id="header-region">
-          <?php print $header; ?>
+<div class="cornell-brand hidden-xs">
+  <div class="container">
+    <div class="row">
+      <div class="cornell-logo">
+        <div class="cu-logo">
+          <img src="/sites/all/themes/bootstrap_cul7/img/layout/cul-logo-white.gif" alt="Cornell University Library">
+          <a id="insignia-link" href="http://www.cornell.edu/">Cornell Insignia</a>
+          <div class="unit-signature-links">
+            <a id="cornell-link" href="http://www.cornell.edu/">Cornell University</a>
+            <a id="unit-link" href="/">Cornell University Library</a>
+          </div>
         </div>
-      <?php endif; ?>
+      </div>
+      <div class="nav-global">
+        <?php print render($page['header']); ?>
+      </div>
+    </div>
+  </div>
+</div>
 
-    </div> <!-- /header -->
+<div class="navbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="mobile-insignia visible-xs">
+        <a href="http://www.cornell.edu"><img src="/sites/all/themes/bootstrap_cul7/img/layout/insignia.png" alt="Cornell University"></a>
+      </div>
+      <a class="navbar-brand visible-xs" href="/">Cornell University Library</a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <div class="visible-xs mobile-search">
+        <form class="form-inline clearfix" role="form" action="http://search.library.cornell.edu/" method="get">
+          <div class="form-group">
+            <label class="sr-only" for="q">Search</label>
+            <input type="text" class="form-control input-sm search-box" id="q" name="q" placeholder="Search everything">
+          </div>
+          <button type="submit" class="btn btn-search-icon btn-sm"><i class="fa fa-search fa-inverse"></i></button>
+        </form>
+      </div>
+      <div class="nav navbar-nav">
+        <?php print render($page['navigation']); ?>
+      </div>
+    </div><!--/.navbar-collapse -->
+  </div>
+</div>
 
-    <div id="container" class="clearfix">
+<?php
+  $announcement  = render($page['announcement']);
+?>
 
-      <?php if (!empty($sidebar_first)): ?>
-        <div id="sidebar-first" class="column sidebar">
-          <?php print $sidebar_first; ?>
-        </div> <!-- /sidebar-first -->
-      <?php endif; ?>
+<?php if ($announcement): ?>
+  <div class="announcement">
+    <div class="container">
+      <?php print render($announcement); ?>
+    </div>
+  </div>
+<?php endif; ?>
 
-      <div id="main" class="column"><div id="main-squeeze">
+<div class="main-content">
+  <div class="container">
+    <div class="row">
+      <div class="breadcrumb-wrapper"><!--TO DO: SEMANTIC-->
+        <?php print render($breadcrumb); ?>
+      </div>
+    </div>
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar  = render($page['sidebar']);
+      $news_sidebar  = render($page['news-sidebar']);
+    ?>
+    <?php // if there is a sidebar, then create two column layout ?>
+    <?php if ($sidebar): ?>
+    <div class="row primary-wrapper">
+      <div class="main-text" id="maincontent">
+        <?php print render($page['highlighted']); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php if ($title): ?>
+          <h1><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+      </div><!--/main-text-->
 
-        <div id="content">
-          <?php if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-          <?php if (!empty($messages)): print $messages; endif; ?>
-          <div id="content-content" class="clearfix">
-            <?php print $content; ?>
-          </div> <!-- /content-content -->
-        </div> <!-- /content -->
+      <div class="sidebar">
+        <?php print render($page['sidebar']); ?>
+        <?php if ($news_sidebar): ?>
+          <div class="callout secondary-feature">
+            <?php print render($page['news-sidebar']); ?>
+          </div>
+        <?php endif; ?>
+      </div><!--/sidebar-->
 
-      </div></div> <!-- /main-squeeze /main -->
+    </div>
 
-      <?php if (!empty($sidebar_second)): ?>
-        <div id="sidebar-second" class="column sidebar">
-          <?php print $sidebar_second; ?>
-        </div> <!-- /sidebar-second -->
-      <?php endif; ?>
+    <?php else: ?>
+      <div class="row primary-wrapper">
+        <?php print render($page['highlighted']); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php if ($title): ?>
+          <h1><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
 
-    </div> <!-- /container -->
+    </div>
 
-    <div id="footer-wrapper">
-      <div id="footer">
-        <?php if (!empty($footer)): print $footer; endif; ?>
-      </div> <!-- /footer -->
-    </div> <!-- /footer-wrapper -->
+  <?php endif; ?>
 
-  </div> <!-- /page -->
+  </div> <!-- /container -->
+</div><!-- /main-content -->
+
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="footer-primary">
+        <?php print render($page['footer-primary']); ?>
+      </div>
+      <div class="footer-secondary">
+        <div class="social-links">
+          <a class="facebook-button" href="http://www.facebook.com/cornelluniversitylibrary" title="Find us on Facebook"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i></a> 
+          <a class="twitter-button" href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.library.cornell.edu%2Findex&amp;region=follow_link&amp;screen_name=Cornell_Library&amp;tw_p=followbutton&amp;variant=2.0" title="Follow us on Twitter"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i></a>
+        </div>
+        <?php print render($page['footer-secondary']); ?>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<?php print render($page['bottom']); ?>
+  
 
 </body>
 </html>
