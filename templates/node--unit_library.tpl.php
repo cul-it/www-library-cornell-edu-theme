383711be9efd3e65148a162c06a68ded05c9dee2
Melissa Wallace <mhk33@cornell.edu>
2014-06-18 19:14:26 +0000

    try to fix semester url
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@868 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/node--unit_library.tpl.php
+++ templates/node--unit_library.tpl.php
@@ -120,7 +120,7 @@
             }
             else {
               $location = urlencode($location);
-              $url = "http://mannservices.mannlib.cornell.edu/LibServices/showLibraryHoursForAcademicYear.do?output=xhtml&location=" . $location;
+              $url = "http://mannservices.mannlib.cornell.edu/LibServices/showLibraryHoursForAcademicSemester.do?output=xhtml&location=" . $location;
               $full_hours = file_get_contents($url);
             }
 
