e686c94df9b10f5db5721171c9d4924495611870
Melissa Wallace <mhk33@cornell.edu>
2014-06-20 16:13:31 +0000

    strip out labels again
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@889 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/node--unit_library.tpl.php
+++ templates/node--unit_library.tpl.php
@@ -125,8 +125,7 @@
                 $full_hours = file_get_contents($url);
 
                 # strip out tags and labels
-                #$patterns = array('/<\/?strong>/','/Location: /','/Description: /', '/Date Range: /', '/style\=".*?"/');
-                $patterns = array('/<\/?strong>/','/style\=".*?"/');
+                $patterns = array('/<\/?strong>/','/Location: /','/Description: /', '/Date Range: /', '/style\=".*?"/');
                 $full_hours = preg_replace($patterns, '', $full_hours);
               }
 
