c1f82a24ed9dceba05bdb1437ab6643f8cc5d38f
Melissa Wallace <mhk33@cornell.edu>
2014-06-24 20:20:42 +0000

    add new footer regions
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@927 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -139,6 +139,7 @@
       <a data-toggle="collapse" data-target=".libraries-collapse" class="btn btn-libraries">
         Libraries <i class="fa fa-chevron-right"></i>
       </a>
+      <!--<#? php print render($page['footer-libraries']); ?>-->
       <div class="libraries-collapse collapse">
         <div class="row">
           <div class="col-sm-4">
@@ -179,7 +180,8 @@
     </div>
     <div class="row">
       <div class="col-sm-2">
-        <h3>Explore</h3>
+        <? php print render($page['footer-links1']); ?>
+        <!-- <h3>Explore</h3>
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About Us</a></li>
@@ -189,57 +191,49 @@
           <li><a href="/research">Research</a></li>
           <li><a href="/services">Services</a></li>
           <li><a href="/ask">Ask a Librarian</a></li>
-        </ul>
+        </ul> -->
       </div>
       <div class="col-sm-2">
-        <h3>Resources</h3>
+        <? php print render($page['footer-links2']); ?>
+        <!-- <h3>Resources</h3>
         <ul>
           <li><a href="http://newcatalog.library.cornell.edu">Catalog</a></li>
           <li><a href="http://cornell.summon.serialssolutions.com/">Articles &amp; Full Text</a></li>
           <li><a href="http://erms.library.cornell.edu/search~S4">Databases</a></li>
           <li><a href="http://erms.library.cornell.edu/">E-journal titles</a></li>
           <li><a href="/visual-resources">Images</a></li>
-        </ul>
+        </ul> -->
       </div>
       <div class="col-sm-2">
-        <h3>Cornell</h3>
+        <? php print render($page['footer-links3']); ?>
+        <!-- <h3>Cornell</h3>
         <ul>
           <li><a href="http://www.cornell.edu">Cornell.edu</a></li>
           <li><a href="http://cuinfo.cornell.edu/">CUinfo</a></li>
-        </ul>
+        </ul> -->
       </div>
       <div class="col-sm-2">
-        <h3>Tools</h3>
+        <? php print render($page['footer-links4']); ?>
+        <!-- <h3>Tools</h3>
         <ul>
           <li><a href="/myacct">My Account</a></li>
           <li><a href="/secure/cu_webauth">Staff Login</a></li>
-        </ul>
+        </ul> -->
       </div>
       <div class="col-sm-4 text-right">
-        <a href="btn btn-cul">Feedback</a>
+        <? php print render($page['footer-feedback']); ?>
+        <!-- <a href="btn btn-cul">Feedback</a> -->
       </div>
-
-      
-      <!-- <div class="footer-primary">
-        <# ?php print render($page['footer-primary']); ?>
-      </div>
-      <div class="footer-secondary">
-        <div class="social-links">
-          <a class="facebook-button" href="http://www.facebook.com/cornelluniversitylibrary" title="Find us on Facebook"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i></a> 
-          <a class="twitter-button" href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.library.cornell.edu%2Findex&amp;region=follow_link&amp;screen_name=Cornell_Library&amp;tw_p=followbutton&amp;variant=2.0" title="Follow us on Twitter"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i></a>
-        </div>
-        <# ?php print render($page['footer-secondary']); ?>
-      </div> -->
-
     </div>
-    <div class="row footer-copyright">
+    <? php print render($page['footer-copyright']); ?>
+    <!-- <div class="row footer-copyright">
       <div class="col-sm-6">
         Copyright
       </div>
       <div class="col-sm-6 text-right">
         social links
       </div>
-    </div>
+    </div> -->
   </div>
 </footer>
 
