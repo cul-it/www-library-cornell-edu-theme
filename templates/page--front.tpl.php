39f77fd1711b283215ae22268630fd4c8c950846
Melissa Wallace <mhk33@cornell.edu>
2014-06-24 16:57:02 +0000

    try out new footer
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@907 29d2e8ee-217f-479a-8a37-9775d62f972e

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -175,14 +175,14 @@
       </div>
       
       <!-- <div class="footer-primary">
-        <?php print render($page['footer-primary']); ?>
+        <# ?php print render($page['footer-primary']); ?>
       </div>
       <div class="footer-secondary">
         <div class="social-links">
           <a class="facebook-button" href="http://www.facebook.com/cornelluniversitylibrary" title="Find us on Facebook"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i></a> 
           <a class="twitter-button" href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fwww.library.cornell.edu%2Findex&amp;region=follow_link&amp;screen_name=Cornell_Library&amp;tw_p=followbutton&amp;variant=2.0" title="Follow us on Twitter"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i></a>
         </div>
-        <?php print render($page['footer-secondary']); ?>
+        <# ?php print render($page['footer-secondary']); ?>
       </div> -->
 
     </div>
