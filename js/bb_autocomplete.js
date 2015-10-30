fedca466adc25be1ee47d325e7dae6566072ae74
Matt Connolly <mjc12@cornell.edu>
2015-10-30 18:10:59 +0000

    Update best bet autocomplete to work with latest changes to Drupal environment
    git-svn-id: https://svn.library.cornell.edu/cul7theme/trunk@1173 29d2e8ee-217f-479a-8a37-9775d62f972e

--- js/bb_autocomplete.js
+++ js/bb_autocomplete.js
@@ -2,16 +2,43 @@
 (function ($) {
   $(document).ready(function() {
 
+    // $('#q').autocomplete({
+    //   source: 'https://bestbets.library.cornell.edu/match/' + $('#q').val(),
+    //   minLength: 2,
+    //   select: function(event, ui) {
+    //     var url = ui.item.url;
+    //     if (url != '#') {
+    //       location.href = url;
+    //     }
+    //   }
+    // })
     $('#q').autocomplete({
-      source: 'https://bestbets.library.cornell.edu/match/' + $('#q').val(),
+      source: function(request, response) {
+        $.ajax({
+          url: 'https://bestbets.library.cornell.edu/match/' + $('#q').val(),
+          success: function(data) {
+            var transformed = $.map(data, function(el) {
+              return {
+                label: el.name,
+                id: el.url
+              };
+            });
+            response(transformed);
+          },
+          error: function() {
+            response([]);
+          }
+        });
+      },
       minLength: 2,
       select: function(event, ui) {
-        var url = ui.item.url;
+        var url = ui.item.id;
         if (url != '#') {
           location.href = url;
         }
       }
     })
+    
     // This next section is just to add the little external link icon (the <i> class) 
     // after the label in the results list!
     // It can be completely removed if all you need is basic autocomplete
