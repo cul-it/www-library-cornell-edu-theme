ae730acbaa444e37f86acb6f6d51939e319d5c6c
Matt Connolly <mattjlc@gmail.com>
2016-04-20 16:08:01 -0400

    Add JS code and CSS for LibCal hours widgets

---
+++ js/hours.js
@@ -0,0 +1,6 @@
+// Insert a LibCal hours widget into a unit library page
+(function ($) {
+  $(document).ready(function() {
+    var week2818 = new jQuery.LibCalWeeklyGrid( jQuery("#s-lc-whw2818"), { iid: 973, lid: 2818,  weeks: 26 }); 
+  });
+})(jQuery);
