39191f0cda225543b47a082b08072fe6e9484bbd
Matt Connolly <mattjlc@icloud.com>
2016-05-04 10:00:48 -0400

    Add check for div to avoid breaking JS

--- js/hours.js
+++ js/hours.js
@@ -2,6 +2,7 @@
 (function ($) {
   $(document).ready(function() {
     var libraryId = jQuery("#s-lc-whw2818").attr('data-hours');
-    var week2818 = new jQuery.LibCalWeeklyGrid( jQuery("#s-lc-whw2818"), { iid: 973, lid: libraryId,  weeks: 26 }); 
+    if (libraryId)
+      var week2818 = new jQuery.LibCalWeeklyGrid( jQuery("#s-lc-whw2818"), { iid: 973, lid: libraryId,  weeks: 26 }); 
   });
 })(jQuery);
